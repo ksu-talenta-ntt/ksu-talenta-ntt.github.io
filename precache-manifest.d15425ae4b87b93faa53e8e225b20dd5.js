cdadee44e8466a858485c867f62a5e67ad84e75e
jordan meta <jordanmeta@mail.com>
2019-05-31 16:37:16 +0800

    Ok

---
+++ precache-manifest.d15425ae4b87b93faa53e8e225b20dd5.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "6107019e119b5cc4f64d",
+    "url": "/ksu-talenta-ntt/css/about.e8fcd2ec.css"
+  },
+  {
+    "revision": "6107019e119b5cc4f64d",
+    "url": "/ksu-talenta-ntt/js/about.687c1234.js"
+  },
+  {
+    "revision": "c19d0a7c973da8f9f6bb",
+    "url": "/ksu-talenta-ntt/css/app.c5216255.css"
+  },
+  {
+    "revision": "c19d0a7c973da8f9f6bb",
+    "url": "/ksu-talenta-ntt/js/app.3d889c99.js"
+  },
+  {
+    "revision": "b4d968ef18d64c565b4a",
+    "url": "/ksu-talenta-ntt/css/chunk-0729a1ab.3171ed81.css"
+  },
+  {
+    "revision": "b4d968ef18d64c565b4a",
+    "url": "/ksu-talenta-ntt/js/chunk-0729a1ab.c83eefec.js"
+  },
+  {
+    "revision": "14adeb9036d5cd758c4e",
+    "url": "/ksu-talenta-ntt/css/chunk-0c46f24d.f5b4ee91.css"
+  },
+  {
+    "revision": "14adeb9036d5cd758c4e",
+    "url": "/ksu-talenta-ntt/js/chunk-0c46f24d.3759281d.js"
+  },
+  {
+    "revision": "27c0b1fd61031a871936",
+    "url": "/ksu-talenta-ntt/css/chunk-0e670de6.d5065d7a.css"
+  },
+  {
+    "revision": "27c0b1fd61031a871936",
+    "url": "/ksu-talenta-ntt/js/chunk-0e670de6.f0fe855d.js"
+  },
+  {
+    "revision": "b530d2eee290259bcf70",
+    "url": "/ksu-talenta-ntt/js/chunk-241f10e5.402d7f3d.js"
+  },
+  {
+    "revision": "7bebad3763d1e97ceb3f",
+    "url": "/ksu-talenta-ntt/css/chunk-40be4e6a.2ba4f6e2.css"
+  },
+  {
+    "revision": "7bebad3763d1e97ceb3f",
+    "url": "/ksu-talenta-ntt/js/chunk-40be4e6a.2225d76d.js"
+  },
+  {
+    "revision": "c73773d7c324663887d4",
+    "url": "/ksu-talenta-ntt/js/chunk-45a65130.6a94dab2.js"
+  },
+  {
+    "revision": "b795c63beea68d57f61f",
+    "url": "/ksu-talenta-ntt/js/chunk-48213456.84d2e337.js"
+  },
+  {
+    "revision": "598a0f48caa23a851eb7",
+    "url": "/ksu-talenta-ntt/js/chunk-6414b436.8ef56403.js"
+  },
+  {
+    "revision": "b647f4d0a5e6f23655ee",
+    "url": "/ksu-talenta-ntt/css/chunk-670e8b08.2ba4f6e2.css"
+  },
+  {
+    "revision": "b647f4d0a5e6f23655ee",
+    "url": "/ksu-talenta-ntt/js/chunk-670e8b08.ef452883.js"
+  },
+  {
+    "revision": "4a7a549faba6ac734737",
+    "url": "/ksu-talenta-ntt/css/chunk-8a5b81de.4e3c8cfd.css"
+  },
+  {
+    "revision": "4a7a549faba6ac734737",
+    "url": "/ksu-talenta-ntt/js/chunk-8a5b81de.c7bf1fe7.js"
+  },
+  {
+    "revision": "f72b0370de00f01b5568",
+    "url": "/ksu-talenta-ntt/css/chunk-c341e31c.145be49c.css"
+  },
+  {
+    "revision": "f72b0370de00f01b5568",
+    "url": "/ksu-talenta-ntt/js/chunk-c341e31c.25a06481.js"
+  },
+  {
+    "revision": "619e8fb2b5a837528544",
+    "url": "/ksu-talenta-ntt/css/chunk-d16fb510.4cb00a35.css"
+  },
+  {
+    "revision": "619e8fb2b5a837528544",
+    "url": "/ksu-talenta-ntt/js/chunk-d16fb510.3faf214d.js"
+  },
+  {
+    "revision": "8c358e701d617a1c4629",
+    "url": "/ksu-talenta-ntt/css/chunk-vendors.b80c345a.css"
+  },
+  {
+    "revision": "8c358e701d617a1c4629",
+    "url": "/ksu-talenta-ntt/js/chunk-vendors.f3e76bad.js"
+  },
+  {
+    "revision": "2944bd60cb70c4bc9b86ea21b975835d",
+    "url": "/ksu-talenta-ntt/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ksu-talenta-ntt/robots.txt"
+  },
+  {
+    "revision": "6a189a4627661c1372d924b718115f3d",
+    "url": "/ksu-talenta-ntt/bg.jpg"
+  },
+  {
+    "revision": "004806ad742bfa51067c52502fe2876e",
+    "url": "/ksu-talenta-ntt/bg3.jpg"
+  },
+  {
+    "revision": "b87f92659e1b862ef0212086e6af7efc",
+    "url": "/ksu-talenta-ntt/bg2.jpg"
+  }
+];
